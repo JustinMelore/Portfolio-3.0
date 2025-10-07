9811858c74a926443e05f59d0437e46cf0042ec8
JustinMelore <melorejustin@gmail.com>
2025-10-07 15:10:12 -0700

    Tweaked Our Light page style and made background transition between several screenshots

--- js/gamePage.js
+++ js/gamePage.js
@@ -6,6 +6,32 @@ window.onload = resizeGameBackground;
  */
 function resizeGameBackground() {
     const gamePageHeader = document.getElementsByClassName("gamePageHeader")[0];
-    gamePageHeader.style.height = window.getComputedStyle(document.getElementsByClassName("slideshowImage")[0])["height"];
-    console.log(`${gamePageHeader.offsetHeight} versus ${document.getElementsByClassName("slideshowImage")[0].offsetHeight}`);
+    gamePageHeader.style.height = window.getComputedStyle(slides[0])["height"];
+    console.log(`${gamePageHeader.offsetHeight} versus ${slides[0].offsetHeight}`);
 }
+
+const slides = document.getElementsByClassName("slideshowImage");
+var currentSlide = resetSlides();
+
+/**
+ * Resets all of the game page's background slides to the beginning state
+ * @returns The index of the background/slide that is currently visible
+ */
+function resetSlides() {
+    console.log(slides.length);
+    slides[0].style.opacity = "100%";
+    for(let i = 1; i < slides.length; i++) {
+        slides[i].style.opacity = "0%"
+    }
+    return 0;
+}
+
+function switchBackground() {
+    slides[currentSlide].style.opacity = "0%";
+    currentSlide++;
+    if(currentSlide >= slides.length) currentSlide = 0;
+    slides[currentSlide].style.opacity = "100%";
+    console.log("Switched background");
+}
+
+setInterval(switchBackground, 5000)
